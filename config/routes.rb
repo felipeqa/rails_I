3fbe307821f510f239fe9eebc11969e73ba26db6
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-06-10 22:26:39 -0300

    add route comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root to: "jobs#premium"
+  root to: 'jobs#premium'
 
-  resources :jobs
+  #cria uma rota dentro das rotas jobs (rota aninhada)
+  resources :jobs do
+    post 'comments', to: 'comments#create'
+  end
   get 'hello/world'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
