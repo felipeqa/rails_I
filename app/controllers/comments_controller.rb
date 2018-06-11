268b6f83e62b140c8c10fe666c59d3717a96bdbc
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-06-10 23:12:55 -0300

    add flash notice and alert

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,11 @@ class CommentsController < ApplicationController
   def create
     @job = Job.find(params[:job_id])
     @comment = @job.comments.build(comment_params)
-    @comment.save
+    if @comment.save
+      flash[:notice] = "Comment was created with sucess!"
+    else
+      flash[:alert] = "Please Fill in all fields to create a comment."
+    end
     redirect_to @job
   end
 
