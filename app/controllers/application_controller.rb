a7cc33fa8ea29d32973e0539b95d1fe742d90f08
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-06-16 21:31:56 -0300

    add method helper current_company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  private
+
+  def current_company
+    @current_company ||= if session[:company_id]
+      Company.find_by_id(session[:company_id])
+    end
+  end
+  helper_method :current_company
 end
