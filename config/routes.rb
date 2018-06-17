455d04cb8aa3b000a3d304f47b47b27b9811eb58
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-06-16 22:10:19 -0300

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get  "/companies/login", to: "login#new"
   post "/companies/login", to: "login#create"
+  delete "/companies/login", to: "login#destroy"
 
   resources :companies, only: [:new, :create]
   delete 'comments/:id', to: 'comments#destroy', as: :comment
