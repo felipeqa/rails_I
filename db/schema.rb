3b04131a83f1ca1b393e404adba2e3d52efa9526
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-06-18 22:29:25 -0300

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_155409) do
+ActiveRecord::Schema.define(version: 2018_06_19_012454) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_06_16_155409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "premium"
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
 end
