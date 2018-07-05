8858969d5c6b3f0e11c3329811a7ed2f3319a812
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-04 22:40:29 -0300

    add .js.erb to respond CommentsController#create as JS

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,20 @@ class CommentsController < ApplicationController
   def create
     @job = Job.find(params[:job_id])
     @comment = @job.comments.build(comment_params)
-    if @comment.save
-      flash[:notice] = "Comment was created with sucess!"
-    else
-      flash[:alert] = "Please Fill in all fields to create a comment."
+
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          flash[:notice] = "Comment was created with sucess!"
+        else
+          flash[:alert] = "Please Fill in all fields to create a comment."
+        end
+        redirect_to @job
+      end
+      format.js do
+        @comment.save
+      end
     end
-    redirect_to @job
   end
 
   def destroy
