2b04b19d037a0d2ddac3956276766ce3f17493c7
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-10 22:25:40 -0300

    update company mailer methods

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -1,13 +1,16 @@
 class CompanyMailer < ApplicationMailer
 
+  default from: 'felipeadsx@gmail.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.company_mailer.new_comment.subject
   #
-  def new_comment
-    @greeting = "Hi"
+  def new_comment(job, comment)
+    @job = job
+    @comment  = comment
+    @company = job.company
 
-    mail to: "to@example.org"
+    mail to: @company.email, subject:  "New comment received"
   end
 end
