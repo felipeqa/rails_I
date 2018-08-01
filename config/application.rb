1158bdb68728991ef5d59f050b58c70ef0de6444
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-31 23:16:41 -0300

    fix new_job and default language pt-BR

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module JobBoard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :'pt-BR'
+    config.encoding = 'utf-8'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
